9dccf5abe8559d5ac72bb556225b37f1dc8402c1
Xumuk0 <131582993+Xumuk0@users.noreply.github.com>
2026-07-09 18:23:12 +0000

    Парсер v2

--- vpn_parser.py
+++ vpn_parser.py
@@ -6,6 +6,7 @@ SOURCE_URL = "https://raw.githubusercontent.com/whoahaow/rjsxrd/refs/heads/main/
 try:
         response = requests.get(SOURCE_URL)
         raw_data = response.text.strip()
+
         try:
                 decoded_data = base64.b64decode(raw_data).decode('utf-8')
                 is_base64 = True
@@ -14,16 +15,20 @@ try:
                 is_base64 = False
                                                                 
         all_nodes = [line for line in decoded_data.splitlines() if line.strip()]
-                                                                            
-        limited_nodes = all_nodes[:100]
-                                                                                        
-        result_text = "\n".join(limited_nodes)
+
+        filtered_nodes = []
+        for line in all_nodes:
+                if "🇷🇺" in line or "White list" in line.lower():
+                        filtered_nodes.append(line)
+        
+        result_text = "\n".join(filtered_nodes)
+
         if is_base64:
                 result_text = base64.b64encode(result_text.encode('utf-8')).decode('utf-8')
                                                                                                                     
-        with open("list_vpn.txt", "w") as f:
+        with open("list.txt", "w") as f:
                 f.write(result_text)
                                                                                                                                             
-        print("Успешно обрезано до 100 серверов!")
+        print(f"Найдено и сохранено {len(filtered_nodes)} серверов")
 except Exception as e:
         print(f"Что-то пошло не так: {e}")
